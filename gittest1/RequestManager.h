371691544dd2c352730b2e5a0a918b5277580e70
Thorsten Neff <thorstenneff@gmx.de>
2013-01-03 22:23:30 +0100

    Added: RequestManager angelegt

---
+++ gittest1/RequestManager.h
@@ -0,0 +1,13 @@
+//
+//  RequestManager.h
+//  gittest1
+//
+//  Created by Thorsten Neff on 1/3/13.
+//  Copyright (c) 2013 talex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RequestManager : NSObject
+
+@end
---
+++ gittest1/RequestManager.m
@@ -0,0 +1,13 @@
+//
+//  RequestManager.m
+//  gittest1
+//
+//  Created by Thorsten Neff on 1/3/13.
+//  Copyright (c) 2013 talex. All rights reserved.
+//
+
+#import "RequestManager.h"
+
+@implementation RequestManager
+
+@end
